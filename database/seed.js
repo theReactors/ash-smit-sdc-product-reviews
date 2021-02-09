722b8cc047b1aef17206ea291b263d72a4c5c2dc
Brian Smit <brianjsmit@gmail.com>
2021-02-09 11:29:31 -0800

    Refactored for deployment

--- database/seed.js
+++ database/seed.js
@@ -3,9 +3,8 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   user: 'postgres',
-  host: 'localhost',
+  host: '18.191.151.153',
   database: 'reviews',
-  password: 'inferno',
   port: 5432,
 });
 
@@ -20,15 +19,15 @@ pool.connect(err => {
 var seedDatabase = () => {
   pool.query(
 
-    `COPY product (imageURL, color) FROM '/Users/aisling/hello/ash-smit-sdc-product-reviews/database/products.csv'DELIMITER ',' CSV HEADER;
+    `COPY product (imageURL, color) FROM '/home/ubuntu/products.csv'DELIMITER ',' CSV HEADER;
     `).catch(e => {console.log(e)});
 
-    pool.query(`COPY review (username, avatar, favorites, body, created, productID) FROM '/Users/aisling/hello/ash-smit-sdc-product-reviews/database/reviews.csv' DELIMITER ',' CSV HEADER;`).catch(e => {console.log(e)});
+    pool.query(`COPY review (username, avatar, favorites, body, created, productID) FROM '/home/ubuntu/reviews.csv' DELIMITER ',' CSV HEADER;`).catch(e => {console.log(e)});
 
   // copy product from products.csv  . then copy review from reviews.csv
 }
 
-// seedDatabase()
+seedDatabase()
 
 var getReviews = (index, callback) => {
   pool.query(`select * from review left join product on productID = product.id where review.productID = ${index}`, callback);
