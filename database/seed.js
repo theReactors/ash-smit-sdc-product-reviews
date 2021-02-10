95070ef77d4a11a207c0aad589ed6014ef10298a
Brian Smit <brianjsmit@gmail.com>
2021-02-10 11:12:03 -0800

    Newly ready for deployment with new relic

--- database/seed.js
+++ database/seed.js
@@ -1,10 +1,12 @@
 const { Pool } = require('pg');
+console.log('hello world')
 
 
 const pool = new Pool({
   user: 'postgres',
-  host: '18.191.151.153',
+  host: '3.141.27.108',
   database: 'reviews',
+  password: 'inferno'
   port: 5432,
 });
 
@@ -16,17 +18,19 @@ pool.connect(err => {
   }
 })
 
+
+
 var seedDatabase = () => {
   pool.query(
 
     `COPY product (imageURL, color) FROM '/home/ubuntu/products.csv'DELIMITER ',' CSV HEADER;
     `).catch(e => {console.log(e)});
 
-    pool.query(`COPY review (username, avatar, favorites, body, created, productID) FROM '/home/ubuntu/reviews.csv' DELIMITER ',' CSV HEADER;`).catch(e => {console.log(e)});
+    pool.query(`\COPY review (username, avatar, favorites, body, created, productID) FROM '/home/ubuntu/reviews.csv' DELIMITER ',' CSV HEADER;`).catch(e => {console.log(e)});
 
 }
 
-seedDatabase()
+// seedDatabase()
 
 var getReviews = (index, callback) => {
   pool.query(`select * from review left join product on productID = product.id where review.productID = ${index}`, callback);
--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
+require('dotenv').config();
 const newRelic = require('newrelic');
 const express = require('express');
 const db = require('../database/seed.js');
 const parser = require('body-parser');
 const path = require('path');
 const compression = require('compression');
+
 
 const app = express();
 
@@ -21,7 +23,8 @@ app.get('/', (req, res) => {
 app.get('/reviews/:id', (req, res) => {
   db.getReviews(req.params.id, (err, reviews) => {
     if (err) {
-      res.send(console.log(err))
+      console.log(err)
+      res.status(500).send(err)
     } else {
       res.send(reviews)
     }
